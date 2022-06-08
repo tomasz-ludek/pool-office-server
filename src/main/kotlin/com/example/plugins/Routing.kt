3f14a30776c4de4047dd58efef7eced180858253
Sergey90K <72564949+Sergey90K@users.noreply.github.com>
2022-06-08 14:20:10 +0300

    Simplify the server response.

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -16,32 +16,32 @@ fun Application.configureRouting() {
             route("/0"){
                 post("/1") {
                     // mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 1 -R /dev/ttyAMA1 1        r 1
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.onRelay(1) ,ContentType.Application.Json,  status = HttpStatusCode.OK) }
+                    return@post call.respondText(simpleModbusRTURelay.onRelay(1) ,ContentType.Application.Json,  status = HttpStatusCode.OK) }
                 post("/0") {
                     //mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 1 -R /dev/ttyAMA1 0     r 1
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.offRelay(1) , ContentType.Application.Json,  status = HttpStatusCode.OK)
+                    return@post call.respondText( simpleModbusRTURelay.offRelay(1) , ContentType.Application.Json,  status = HttpStatusCode.OK)
                 }
             }
             route("/5"){
                 post("/1") {
                     //mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 4 -R /dev/ttyAMA1 1        r4
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.onRelay(4) , ContentType.Application.Json,  status = HttpStatusCode.OK)
+                    return@post call.respondText( simpleModbusRTURelay.onRelay(4) , ContentType.Application.Json,  status = HttpStatusCode.OK)
                 }
                 post("/0") {
                     //mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 4 -R /dev/ttyAMA1 0       r 4
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.offRelay(4) , ContentType.Application.Json,  status = HttpStatusCode.OK) }
+                    return@post call.respondText( simpleModbusRTURelay.offRelay(4) , ContentType.Application.Json,  status = HttpStatusCode.OK) }
             }
             route("/256"){
                 post("/1") {
                     // mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 256 -R /dev/ttyAMA1 1    r 256
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.onRelay(256) , ContentType.Application.Json,  status = HttpStatusCode.OK)
+                    return@post call.respondText( simpleModbusRTURelay.onRelay(256) , ContentType.Application.Json,  status = HttpStatusCode.OK)
 //            or simpleModbusRTURelay.onRelay(2)
 //               simpleModbusRTURelay.onRelay(5)
 //               simpleModbusRTURelay.onRelay(6)
                 }
                 post("/0") {
                     // mbpoll -v -a 1 -b 9600 -m rtu -t 0 -P none -r 256 -R /dev/ttyAMA1 0      r 256
-                    return@post call.respondText("Test work rezalt - " +  simpleModbusRTURelay.offRelay(256) , ContentType.Application.Json, status = HttpStatusCode.OK)
+                    return@post call.respondText( simpleModbusRTURelay.offRelay(256) , ContentType.Application.Json, status = HttpStatusCode.OK)
                 //            or simpleModbusRTURelay.offRelay(2)
 //                           simpleModbusRTURelay.offRelay(5)
 //                           simpleModbusRTURelay.offRelay(6)
